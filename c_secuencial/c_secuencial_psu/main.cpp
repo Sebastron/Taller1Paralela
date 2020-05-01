858287425653f5df5f0e814eaa6b991efd461af6
Sebastron <sebastiang2806@gmail.com>
2020-04-30 21:34:53 -0400

    C++ secuencial hecho, sin pruebas

--- c_secuencial/c_secuencial_psu/main.cpp
+++ c_secuencial/c_secuencial_psu/main.cpp
@@ -2,66 +2,43 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
-#include <omp.h>
-
 
 using namespace std;
 
-/**
- * Función que muestra los participantes del grupo
- */
-void participante();
 
-/**
- * 
- * @param inferior límite menor para buscar
- * @param superior límite mayor para buscar
- * @return un número entre dichos límites
- */
-int aleatoreo(int inferior, int superior);
+void integrantes(){
+    std::cout << std::endl << "Integrantes:" << std::endl;
+    std::cout << "Sebastián Garrido Valenzuela" << std::endl;
+    std::cout << "Ramiro Uribe Garrido" << std::endl;
+    std::cout << "Javier Gálvez González" << std::endl;
+}   
 
-/**
- * Taller computacional
- * @param argc cantidad de argumentos
- * @param argv argumentos
- * @return El código de salida del programa
- */
+
+
+int aleatoreo(int inferior, int superior){
+    return (rand() % (superior - (inferior + 1))) +inferior;
+}
+
+
 int main(int argc, char** argv) {
     srand((unsigned int) time(0));
     std::string pyc(";");
-
-    std::ofstream archivoSalida("puntajes.csv");
-
-#pragma omp parallel 
-    {
-
-#pragma omp for
-        for (unsigned long rut = 14916641; rut <= 19932391; rut++) {
-            int nem = aleatoreo(475, 750);
-            int ranking = aleatoreo(475, 750);
-            int matematica = aleatoreo(475, 750);
-            int lenguaje = aleatoreo(475, 750);
-            int ciencias = aleatoreo(475, 750);
-            int historia = aleatoreo(475, 750);
-
-            std::string linea;
-            linea = std::to_string(rut) + pyc + std::to_string(nem) + pyc + std::to_string(ranking) + pyc + std::to_string(matematica) + pyc + std::to_string(lenguaje) + pyc + std::to_string(ciencias) + pyc + std::to_string(historia);
-
-#pragma omp critical
-            archivoSalida << linea << std::endl;
-        }
+    std::cout << "Taller 1: C++, OpenMP y Dart" << std::endl;
+    std::ofstream archivoSalida("prompuntajes.csv");
+    for (unsigned long rut = 14916641; rut <= 19932391; rut++) {
+        float nem = aleatoreo(475, 750);
+        float ranking = aleatoreo(475, 750);
+        float matematica = aleatoreo(475, 750);
+        float lenguaje = aleatoreo(475, 750);
+        float ciencia = aleatoreo(475, 750);
+        float historia = aleatoreo(475, 750);
+        float prompuntaje = (nem + ranking + matematica + lenguaje + ciencia + historia)/6;
+        std::string linea;
+        linea = std::to_string(rut) + ";" + std::to_string(prompuntaje);
+        archivoSalida << linea << std::endl;
     }
     archivoSalida.close();
 
-    participante();
+    integrantes();
     return EXIT_SUCCESS;
-}
-
-void participante() {
-    std::cout << std::endl << "=== Tarea ===" << std::endl;
-    std::cout << std::endl << "Sebastián Salazar Molina" << std::endl; // Reemplazar por su nombre
-}
-
-int aleatoreo(int inferior, int superior) {
-    return (rand() % (superior - (inferior + 1))) +inferior;
 }
